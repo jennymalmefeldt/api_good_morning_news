a04bfe799d520d38c9ad3235c2b67b3cd27d1aed
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-08 14:24:24 +0200

    Installing active_model_serializers gem, generating article index serializer, configuring the serializer, modifying the response for the index action accordingly, adding tests for diaplying title and teaser but not text.

--- spec/requests/api/v1/articles/index_spec.rb
+++ spec/requests/api/v1/articles/index_spec.rb
@@ -13,8 +13,20 @@ RSpec.describe "GET /api/v1/articles", type: :request do
       expect(response.status).to eq 200
     end
 
-    it "is expected to return a list of 3 articles" do
+    it "is expected to return 3 articles" do
       expect(response_json["articles"].count).to eq 3
+    end
+
+    it "is expected to return a specific article title" do
+      expect(response_json["articles"].first["title"]).to eq "MyString"
+    end
+
+    it "is expected to return a specific article teaser" do
+      expect(response_json["articles"].second["teaser"]).to eq "MyText"
+    end
+
+    it "is expected not to return an articles text" do
+      expect(response_json["articles"].second["text"]).to eq nil
     end
   end
 end
