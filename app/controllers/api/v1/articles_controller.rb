21ac6d93ec04fe39476158a6a0cdd7a6f20e626c
sara <saralundkvist86@gmail.com>
2020-10-09 10:37:06 +0200

    stringinterpolates id in article url, using ActiveRecord for sending errormessage. happy and sad path goes green

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::ArticlesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_active_record_error
+
   def index
     articles = Article.all
-
     render json: articles, each_serializer: ArticlesIndexSerializer
   end
 
   def show
     article = Article.find(params[:id])
+    render json: article, serializer: ArticleShowSerializer
+  end
 
-    render json: article, serializer: ArticleShowSerializer 
+  private
+
+  def render_active_record_error
+    render json: { error_message: 'Sorry we can not find that article' }, status: 404
   end
 end
--- spec/requests/api/v1/articles/show_spec.rb
+++ spec/requests/api/v1/articles/show_spec.rb
@@ -3,10 +3,7 @@ RSpec.describe 'GET /api/v1/articles', type: :request do
 
   describe 'successfully' do
     before do
-      get '/api/v1/articles',
-          params: {
-            article_id: article.id
-          }
+      get "/api/v1/articles/#{article.id}"
     end
 
     it 'should respond with 200 status' do
@@ -14,18 +11,15 @@ RSpec.describe 'GET /api/v1/articles', type: :request do
     end
 
     it 'should respond with article_id' do
-      expect(response_json["articles"].first["id"]).to eq Article.first.id
+      expect(response_json['article']['id']).to eq article.id
     end
-
   end
 
   describe 'unsuccessfully' do
     before do
-      get '/api/v1/articles',
-           params: {
-             article_id: '760'
-           }
+      get "/api/v1/articles/700"
     end
+
     it 'should respond with 404 status' do
       expect(response).to have_http_status 404
     end
