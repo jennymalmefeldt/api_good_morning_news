9c07d96d812b4458f9d46f2fedb45b84f5e72393
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-11 18:45:54 +0200

    Installing devise_token_auth created a factorybot, generating user model, adding creat action in routes

---
+++ config/initializers/devise_token_auth.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+DeviseTokenAuth.setup do |config|
+  config.token_cost = Rails.env.test? ? 4 : 10
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-Rspec.describe Journalist, type: :model do
+RSpec.describe User, type: :model do
   it 'should have a valid factory' do
-    expect(FactoryBot.create(:journalist)).to be_valid
+    expect(FactoryBot.create(:user)).to be_valid
   end
-  
+
   describe 'Database table' do
     it { is_expected.to have_db_column :encrypted_password }
     it { is_expected.to have_db_column :email }
@@ -14,23 +14,22 @@ Rspec.describe Journalist, type: :model do
 
   describe 'validation' do
     it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_confirmation_of :password }
   end
 
-  context 'should not have a invalid email adress' do
+  context 'should not have a invalid email address' do
     emails = ['ayaf@ dt.com', '@example.com', 'test me @yao.com',
               'asaf@example', 'ddd@.d. .d', 'ddd@.d']
-
     email.each do |email|
       it { is_expected.not_to allow_value(email).for(:email) }
     end
   end
 
-  context 'should have a valid email adress' do
+  context 'should have a valid email address' do
     emails = ['ayaf@dt.com', 'jenny@example.com', 'testme@yao.com',
               'asaf@example.com']
     email.each do |email|
       it { is_expected.to allow_value(email).for(:email) }
-    end     
+    end
   end
 end
