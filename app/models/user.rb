33dc5b0e78a88bf072d7255a4b248f8d89b0aa2d
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-14 09:22:54 +0200

    adding enum to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  enum role: [:journalist]
+  has_many :articles
+
 end
