0d0c165ebd5c4ca6f809aa19d9822860f52c2771
FacundoOs <osores.facundo@gmail.com>
2020-10-18 19:56:34 +0200

    modifyies number of visible characters in premium articles to match with front

--- app/serializers/articles/show_serializer.rb
+++ app/serializers/articles/show_serializer.rb
@@ -2,6 +2,6 @@ class Articles::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :teaser, :content, :category, :premium
 
   def content
-    object.premium ? object.content[0...100] : object.content
+    object.premium ? object.content[0...299] : object.content
   end
 end
