c22cbf2216ce3ab6dc9cb5465918aad8ca65ebb1
FacundoOs <osores.facundo@gmail.com>
2020-10-18 13:02:21 +0200

    add test env method for adding paid onvoice for subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::SubscriptionsController < ApplicationController
       # when we have the subscriber we can create the subscription
       subscription = Stripe::Subscription.create({ customer: customer_id, plan: "gold_subscription" })
       # now we have to check if the invoice was created and the payment is done
+      test_env?(customer_id, subscription)
       status = Stripe::Invoice.retrieve(subscription.latest_invoice).paid
       # .paid is true or false
 
@@ -25,9 +26,16 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   private
 
+  def test_env?(customer, subscription)
+    if Rails.env.test?
+      invoice = Stripe::Invoice.create({ customer: customer, subscription: subscription.id, paid: true })
+      subscription.latest_invoice = invoice.id
+    end
+  end
+
   def get_customer(stripe_token)
     customer = Stripe::Customer.list(email: current_user.email).data.first
-    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripe_token, currency: 'sek' })
+    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripe_token, currency: "sek" })
     customer.id
     # we are asking if the user exists. Run the line 15 and if it could not find anything, continue with the next line. || is an OR. In this line create a new customer. In the next return the customer id
   end
