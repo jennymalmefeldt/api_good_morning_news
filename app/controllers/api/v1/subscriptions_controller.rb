fe6570a433156c53f1b2ae693e3791300a24837e
FacundoOs <osores.facundo@gmail.com>
2020-10-18 12:03:27 +0200

    add payment flow with stripe in subscription create

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,4 +1,38 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    begin
+      customer_id = get_customer(params[:stripeToken])
+      # when we have the subscriber we can create the subscription
+      subscription = Stripe::Subscription.create({ customer: customer_id, plan: "gold_subscription" })
+      # now we have to check if the invoice was created and the payment is done
+      status = Stripe::Invoice.retrieve(subscription.latest_invoice).paid
+      # .paid is true or false
+
+      if status === true
+        current_user.update_attribute(:role, "subscriber")
+        render json: { message: "Subscription succesfully created!" }, status: :created
+      else
+        # it's still a possibility to reach this part
+        render_stripe_error("Insufficient balance")
+      end
+    rescue => error
+      # if begin or get_customer brakes will always come here and not to else
+      binding.pry
+    end
+  end
+
+  private
+
+  def get_customer(stripe_token)
+    customer = Stripe::Customer.list(email: current_user.email).data.first
+    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripe_token })
+    customer.id
+    # we are asking if the user exists. Run the line 15 and if it could not find anything, continue with the next line. || is an OR. In this line create a new customer. In the next return the customer id
+  end
+
+  def render_stripe_error(error)
+    render json: { message: "The subscription was not made. #{error}" }, status: 402
   end
 end
