ecc4159b641319f6a1fe4cc06da2500c0f8ae09d
FacundoOs <osores.facundo@gmail.com>
2020-10-19 09:42:46 +0200

    subscriber can see the complete article. Modifies seed file

--- app/serializers/articles/show_serializer.rb
+++ app/serializers/articles/show_serializer.rb
@@ -2,6 +2,6 @@ class Articles::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :teaser, :content, :category, :premium
 
   def content
-    object.premium ? object.content[0...299] : object.content
+    object.premium && (current_user.nil? || current_user.role == "user") ? object.content[0..299] : object.content
   end
 end
