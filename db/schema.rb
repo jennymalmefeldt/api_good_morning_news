c338304dd358e0c5d3169702a978f4534a017cf6
emiliano-ma <emilianomainero@gmail.com>
2020-10-25 09:41:25 +0100

    Adds column :location to Article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_194053) do
+ActiveRecord::Schema.define(version: 2020_10_25_083804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_194053) do
     t.integer "category"
     t.bigint "journalist_id"
     t.boolean "premium"
+    t.string "location"
     t.index ["journalist_id"], name: "index_articles_on_journalist_id"
   end
 
